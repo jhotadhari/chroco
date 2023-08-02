c2bb4c28ba39f27619e2edc35e87b22ca2c2cc9b
jhotadhari <tellme@waterproof-webdesign.info>
2023-08-02 16:14:36 +0200

    Try to restrict app to single instance
    Do it this way: https://github.com/electron/electron/issues/648#issuecomment-966669259

--- src/main/main.js
+++ src/main/main.js
@@ -8,35 +8,51 @@ const {
 } = require( './setupApi' );
 const setupWindowMenu = require( './setupWindowMenu' );
 
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if ( require( 'electron-squirrel-startup' ) ) {
 	app.quit();
 }
 
+let mainWindow = null;
 const createWindow = () => {
-	// Create the browser window.
-	const mainWindow = new BrowserWindow( {
-		width: 1600,
-		height: 800,
-		webPreferences: {
-			nodeIntegration: false, // is default value after Electron v5
-			contextIsolation: true, // protect against prototype pollution
-			enableRemoteModule: false, // turn off remote
-			preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
-		},
-	} );
+	if ( null === mainWindow ) {
+		// Create the browser window.
+		mainWindow = new BrowserWindow( {
+			width: 1600,
+			height: 800,
+			webPreferences: {
+				nodeIntegration: false, // is default value after Electron v5
+				contextIsolation: true, // protect against prototype pollution
+				enableRemoteModule: false, // turn off remote
+				preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+			},
+		} );
 
-	// and load the index.html of the app.
-	mainWindow.loadURL( MAIN_WINDOW_WEBPACK_ENTRY );
+		// and load the index.html of the app.
+		mainWindow.loadURL( MAIN_WINDOW_WEBPACK_ENTRY );
 
-	mainWindow.webContents.on( 'did-stop-loading', () => {
-		mainWindow.setTitle( app.getName() + ' - offline time tracker' );
-	} );
+		mainWindow.webContents.on( 'did-stop-loading', () => {
+			mainWindow.setTitle( app.getName() + ' - offline time tracker' );
+		} );
 
-	setupWindowMenu( mainWindow );
-	setupApi();
+		setupWindowMenu( mainWindow );
+		setupApi();
+	}
 };
+
+const gotTheLock = app.requestSingleInstanceLock();
+if ( ! gotTheLock ) {
+	app.quit();
+}
+
+app.on( 'second-instance', ( event, commandLine, workingDirectory ) => {
+	if ( mainWindow ) {
+		if ( mainWindow.isMinimized() ) {
+			mainWindow.restore();
+		}
+		mainWindow.focus();
+	}
+} );
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
